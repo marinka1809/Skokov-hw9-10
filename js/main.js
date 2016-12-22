22dcbbf00b69b50ab9022e4b363951d7e0189b88
Maryna Paliy <marinka18_09@ukr.net>
2016-12-22 16:23:26 +0200

    Ready Skokov error corrected

---
+++ js/main.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function(){
+    $('#masonry').masonry({
+        // options
+        itemSelector: '.item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+});
+
+jQuery(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+jQuery(document).ready(function(){
+    $('.sl').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
