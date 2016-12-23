6a345cce62c89ab5409375fbd1285acca7847897
Maryna Paliy <marinka18_09@ukr.net>
2016-12-23 17:26:35 +0200

    Add 404.html and compress images

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp        = require('gulp'),
-    sass        = require('gulp-sass');
+    sass        = require('gulp-sass'),
     browserSync = require('browser-sync'), // Подключаем Browser Sync
     concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-    uglify      = require('gulp-uglifyjs'); // Подключаем gulp-uglifyjs (для сжатия JS)
+    uglify      = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
     cssnano     = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
     rename      = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
-    del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
-    imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-    pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-    cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
+    del         = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    imagemin    = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant    = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    cache       = require('gulp-cache'), // Подключаем библиотеку кеширования
     autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 
@@ -51,11 +51,51 @@ gulp.task('css-libs', ['sass'], function() {
         .pipe(gulp.dest('stylesheets')); // Выгружаем в папку css
 });
 
+gulp.task('img', function() {
+    return gulp.src('img/**/*') // Берем все изображения
+        .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('img')); //
+});
+
 gulp.task('watch', ['browser-sync','css-libs','scripts', 'sass'], function () {
     gulp.watch('sass/*.+(scss|sass)', ['sass']);
     gulp.watch('*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 
 });
+
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+
+    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+        'stylesheets/main.css',
+        'stylesheets/libs.min.css'
+    ])
+        .pipe(gulp.dest('dist/stylesheets'))
+
+    var buildFonts = gulp.src('fonts/**/*') // Переносим шрифты в продакшен
+        .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('js/**/*') // Переносим скрипты в продакшен
+        .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('*.html') // Переносим HTML в продакшен
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clear', function () {
+    return cache.clearAll();
+})
+
+gulp.task('default', ['watch']);
+
 
 
